417788061ba2ecf6bae2f25d627486fc0ddf4281
ashkeep <1393067551@qq.com>
2020-06-03 16:54:00 +0800

    first commit

--- src/main/java/com/lk/movie/controller/UserController.java
+++ src/main/java/com/lk/movie/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import redis.clients.jedis.Jedis;
 
-import java.rmi.Remote;
 
 
 /**
